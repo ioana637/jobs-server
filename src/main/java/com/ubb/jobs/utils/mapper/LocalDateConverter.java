371a3e6aeb4b297236488853b56e65388d02a9f2
Bogdan Demian <ds.bogdan7@gmail.com>
2018-11-14 20:29:55 +0200

    fixed a bug

--- src/main/java/com/ubb/jobs/utils/mapper/LocalDateConverter.java
+++ src/main/java/com/ubb/jobs/utils/mapper/LocalDateConverter.java
@@ -10,11 +10,11 @@ public class LocalDateConverter implements
         AttributeConverter<LocalDate, Date> {
     @Override
     public java.sql.Date convertToDatabaseColumn(LocalDate entityValue) {
-        return java.sql.Date.valueOf(entityValue);
+        return entityValue == null ? null : java.sql.Date.valueOf(entityValue);
     }
 
     @Override
     public LocalDate convertToEntityAttribute(java.sql.Date databaseValue) {
-        return databaseValue.toLocalDate();
+        return databaseValue == null ? null : databaseValue.toLocalDate();
     }
 }
