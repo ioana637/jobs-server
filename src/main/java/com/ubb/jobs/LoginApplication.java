030179eb82199706e124a591d415c590f27d574f
Bogdan Demian <ds.bogdan7@gmail.com>
2019-01-18 17:30:04 +0200

    open browser

--- src/main/java/com/ubb/jobs/LoginApplication.java
+++ src/main/java/com/ubb/jobs/LoginApplication.java
@@ -4,11 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.io.IOException;
+
 @SpringBootApplication
 @EnableScheduling
 public class LoginApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoginApplication.class, args);
+        Runtime rt = Runtime.getRuntime();
+        try {
+            rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
